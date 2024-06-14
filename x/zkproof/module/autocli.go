9a59e2f2b1ff04337d8891d98c397256217ca6fc
yp945 <pan107104@outlook.com>
2024-06-14 21:22:46 +0800

    add all proof types query

--- x/zkproof/module/autocli.go
+++ x/zkproof/module/autocli.go
@@ -31,6 +31,13 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{},
 				},
 
+				{
+					RpcMethod:      "AllProofTypes",
+					Use:            "all-proof-types",
+					Short:          "Query all-proof-types",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{},
+				},
+
 				// this line is used by ignite scaffolding # autocli/query
 			},
 		},
