b0db8475f359cfeb8ece71baea2f11db55fc8c91
fiamma-buildler <panos@fiammachain.io>
2024-06-26 13:32:22 +0800

    bugfix makefile build linux

---
+++ multi_node_setup.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+: "${CHAIN_ID:=fiamma-testnet-1}"
+: "${PASSWORD:=password}"
+token="fiamma"
+initial_balance=1000000000000
+initial_faucet_balance=1000000000
+initial_stake=10000000
+minimum_gas_price=0.0001
+
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <node1> [<node2> ...]"
+    exit 1
+fi
+
+echo "Creating directories for nodes..."
+rm -rf testnet-nodes
+for node in "$@"; do
+    mkdir -p testnet-nodes/$node
+done
+
+node_ids=()
+
+for node in "$@"; do
+    echo "Initializing $node..."
+    docker run --rm -v $(pwd)/testnet-nodes/$node:/root/.fiamma -it fiammachain/fiammad init fiamma_$node --chain-id $CHAIN_ID
+    
+    docker run --rm -it -v $(pwd)/testnet-nodes/$node:/root/.fiamma --entrypoint sed fiammachain/fiammad -i 's/"stake"/"'$token'"/g' /root/.fiamma/config/genesis.json 
+    docker run --rm -v $(pwd)/testnet-nodes/$node:/root/.fiamma -it fiammachain/fiammad config set app minimum-gas-prices "$minimum_gas_price$token"
+    docker run --rm -v $(pwd)/testnet-nodes/$node:/root/.fiamma -it fiammachain/fiammad config set app pruning "nothing" 
+
+
+    node_id=$(docker run --rm -i -v $(pwd)/testnet-nodes/$node:/root/.fiamma fiammachain/fiammad tendermint show-node-id)
+    node_ids+=($node_id)
+
+    echo "Node ID for $node: $node_id"
+done
+
+
+for (( i=1; i <= "$#"; i++ )); do
+    echo "Creating key for ${!i} user..."
+    printf "$PASSWORD\n$PASSWORD\n" | docker run --rm -i -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma fiammachain/fiammad keys --keyring-backend file --keyring-dir /root/.fiamma/keys add val_${!i} > /dev/null 2> ./testnet-nodes/${!i}/mnemonic.txt
+
+    val_address=$(echo $PASSWORD | docker run --rm -i -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma fiammachain/fiammad keys --keyring-backend file --keyring-dir /root/.fiamma/keys show val_${!i} --address)
+    echo "val_${!i} address: $val_address"
+    echo "val_${!i} mnemonic: $(cat ./testnet-nodes/${!i}/mnemonic.txt)"
+
+    echo "Giving val_${!i} some tokens..."
+    if [ $i -eq 1 ]; then
+        faucet_initial_balance=$((initial_faucet_balance + initial_stake))
+        docker run --rm -it -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma fiammachain/fiammad genesis add-genesis-account $val_address $faucet_initial_balance$token
+    else
+        docker run --rm -it -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma fiammachain/fiammad genesis add-genesis-account $val_address $initial_balance$token
+    fi
+
+    if [ $((i+1)) -le "$#" ]; then
+        j=$((i+1))
+        cp testnet-nodes/${!i}/config/genesis.json testnet-nodes/${!j}/config/genesis.json
+    elif [ $# != 1 ] && [ $((i+1)) -gt $# ]; then
+        cp testnet-nodes/${!i}/config/genesis.json testnet-nodes/$1/config/genesis.json
+    fi
+done
+
+
+
+for (( i=1; i <= "$#"; i++ )); do
+    echo "Giving val_${!i} some stake..."
+    echo $PASSWORD | docker run --rm -i -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma fiammachain/fiammad genesis gentx val_${!i} $initial_stake$token --keyring-backend file --keyring-dir /root/.fiamma/keys --account-number 0 --sequence 0 --chain-id $CHAIN_ID --gas 1000000 --gas-prices $minimum_gas_price$token
+
+    if [ $i -gt 1 ]; then
+        cp testnet-nodes/${!i}/config/gentx/* testnet-nodes/$1/config/gentx/
+    fi
+
+    if [ $((i+1)) -le "$#" ]; then
+        j=$((i+1))
+        cp testnet-nodes/${!i}/config/genesis.json testnet-nodes/${!j}/config/genesis.json
+    elif [ $# != 1 ] && [ $((i+1)) -gt $# ]; then
+        cp testnet-nodes/${!i}/config/genesis.json testnet-nodes/$1/config/genesis.json
+    fi
+done
+
+echo "Collecting genesis transactions..."
+docker run --rm -it -v $(pwd)/testnet-nodes/$1:/root/.fiamma fiammachain/fiammad genesis collect-gentxs > /dev/null
+
+if ! docker run --rm -it -v $(pwd)/testnet-nodes/$1:/root/.fiamma fiammachain/fiammad genesis validate-genesis; then
+    echo "Invalid genesis"
+    exit 1
+fi
+
+echo "Copying genesis file to other nodes..."
+for node in "${@:2}"; do
+    cp testnet-nodes/$1/config/genesis.json testnet-nodes/$node/config/genesis.json
+done
+
+echo "Setting node addresses in config..."
+for (( i=1; i <= "$#"; i++ )); do
+    other_addresses=()
+    for (( j=1; j <= "$#"; j++ )); do
+        if [ $j -ne $i ]; then
+            other_addresses+=("${node_ids[$j - 1]}@${!j}:26656")
+        fi
+    done
+    other_addresses=$(IFS=,; echo "${other_addresses[*]}")
+    #Peer configuration
+    docker run --rm -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma -it fiammachain/fiammad config set config p2p.persistent_peers "$other_addresses" --skip-validate
+    #RPC configuration
+    docker run --rm -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma -it fiammachain/fiammad config set config rpc.laddr "tcp://0.0.0.0:26657" --skip-validate
+    #Explorer configuration
+    docker run --rm -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma -it fiammachain/fiammad config set config rpc.cors_allowed_origins '["*"]' --skip-validate 
+    docker run --rm -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma -it fiammachain/fiammad config set app api.enable true --skip-validate 
+    docker run --rm -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma -it fiammachain/fiammad config set app api.enabled-unsafe-cors true --skip-validate 
+    docker run --rm -v $(pwd)/testnet-nodes/${!i}:/root/.fiamma -it fiammachain/fiammad config set app api.address "tcp://0.0.0.0:1317" --skip-validate
+done
+
+
+echo "Setting up docker compose..."
+rm -f ./testnet-nodes/docker-compose.yml
+printf "version: '3.7'\nnetworks:\n  net-public:\nservices:\n" > ./testnet-nodes/docker-compose.yml
+for node in "$@"; do
+    printf "  fiammad-$node:\n    command: start\n    image: fiammachain/fiammad\n    container_name: $node\n    volumes:\n      - ./$node:/root/.fiamma\n    networks:\n      - net-public\n" >> ./testnet-nodes/docker-compose.yml
+    if [ $node == "$1" ]; then
+        printf "    ports:\n      - 0.0.0.0:26657:26657\n" >> ./testnet-nodes/docker-compose.yml
+    fi
+    printf "\n" >> ./testnet-nodes/docker-compose.yml
+done
