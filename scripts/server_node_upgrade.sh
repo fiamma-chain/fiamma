aa303a8b040922c5d0809c81a4e4808c32ea8dda
fiamma-buildler <panos@fiammachain.io>
2024-07-08 21:59:31 +0800

    feat: 🎸 add query verify result rpc

---
+++ scripts/server_node_upgrade.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 [prod|test] binary_release_tag"
+    exit 1
+fi
+
+if [ "$1" = "prod" ]; then
+    nodes=("node1" "node2" "node3" "node4")
+    nodes_ips=("172.31.31.70" "172.31.27.65" "172.31.17.72" "172.31.26.39") 
+    servers=("ubuntu@18.182.20.173" "ubuntu@35.73.202.182" "ubuntu@35.74.243.172" "ubuntu@18.179.17.155")
+    read -p "Are you sure you want to deploy in production? (y/n): " answer
+    if [ "$answer" != "y" ]; then
+        exit 0
+    fi
+elif [ "$1" = "test" ]; then
+    nodes=("node1" "node2" "node3" "node4")
+    nodes_ips=("172.31.31.70" "172.31.27.65" "172.31.17.72" "172.31.26.39") 
+    servers=("ubuntu@18.182.20.173" "ubuntu@35.73.202.182" "ubuntu@35.74.243.172" "ubuntu@18.179.17.155")
+else
+    echo "Usage: $0 [prod|test] binary_release_tag"
+    exit 1
+fi
+
+
+echo "Downloading source code into servers..."
+for server in "${servers[@]}"; do
+    ssh $server "rm -rf /home/ubuntu/fiamma"
+    ssh $server "git clone https://github.com/fiamma-chain/fiamma.git /home/ubuntu/fiamma"
+    ssh $server "cd /home/ubuntu/fiamma && git checkout $2 && source /home/ubuntu/.profile && make install"
+    echo "Source code downloaded into $server successfully"
+done
+
+
+echo "Sending directories to servers..."
+for i in "${!servers[@]}"; do  
+    ssh ${servers[$i]} "rm -rf /home/ubuntu/.fiamma"
+    scp -r "testnet-nodes/${nodes[$i]}" "${servers[$i]}:/home/ubuntu/.fiamma"
+
+    ## Config Cosmovisor for chain upgrade
+    ssh ${servers[$i]} "mkdir -p ~/.fiamma/cosmovisor/upgrades/$2/bin/"
+    ssh ${servers[$i]} "cp /home/ubuntu/go/bin/fiammad /home/ubuntu/.fiamma/cosmovisor/genesis/bin/fiammad"
+done
+
+
+cd ..
+
--- x/zkpverify/types/query.pb.gw.go
+++ x/zkpverify/types/query.pb.gw.go
@@ -124,6 +124,42 @@ func local_request_Query_ProofData_0(ctx context.Context, marshaler runtime.Mars
 }
 
 var (
+	filter_Query_VerifyResult_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_Query_VerifyResult_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryVerifyResultRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_VerifyResult_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.VerifyResult(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Query_VerifyResult_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryVerifyResultRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_VerifyResult_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.VerifyResult(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+var (
 	filter_Query_BitVMWitness_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
@@ -231,6 +267,29 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 		}
 
 		forward_Query_ProofData_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_Query_VerifyResult_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Query_VerifyResult_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Query_VerifyResult_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -358,6 +417,26 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_Query_VerifyResult_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Query_VerifyResult_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Query_VerifyResult_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_Query_BitVMWitness_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -388,6 +467,8 @@ var (
 
 	pattern_Query_ProofData_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"fiamma", "zkpverify", "get_proof_data"}, "", runtime.AssumeColonVerbOpt(false)))
 
+	pattern_Query_VerifyResult_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"fiamma", "zkpverify", "get_verify_result"}, "", runtime.AssumeColonVerbOpt(false)))
+
 	pattern_Query_BitVMWitness_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"fiamma", "zkpverify", "get_bitvm_witness"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
@@ -397,6 +478,8 @@ var (
 	forward_Query_PendingProof_0 = runtime.ForwardResponseMessage
 
 	forward_Query_ProofData_0 = runtime.ForwardResponseMessage
+
+	forward_Query_VerifyResult_0 = runtime.ForwardResponseMessage
 
 	forward_Query_BitVMWitness_0 = runtime.ForwardResponseMessage
 )
